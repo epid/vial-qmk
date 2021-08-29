c03063ef627fe94cb9830ce92ee5811efc99b51b
Jared G <jared@crypto.cx>
2021-08-28 21:51:36 -0500

    Customized my personal babyv Vial keymap

--- keyboards/eyeohdesigns/babyv/keymaps/vial/keymap.c
+++ keyboards/eyeohdesigns/babyv/keymaps/vial/keymap.c
@@ -1,45 +1,68 @@
-/* Copyright 2020 Eye Oh! Designs
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-   
-    QWERTY,
-    FUNCTN,
-    NUMBRS,
-};
+
+#define CTL_ESC CTL_T(KC_ESC)
+#define DS_PRSR LALT(KC_UP)           // Previous discord server
+#define DS_NXSR LALT(KC_DOWN)         // Next discord server
+#define DS_PRUN LALT(LSFT(KC_UP))     // Previous unread discord msg
+#define DS_NXUN LALT(LSFT(KC_DOWN))   // Next unread discord msg
+#define DS_READ KC_ESC
+#define DS_SRED LSFT(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [QWERTY] = LAYOUT_1u(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,     KC_P,     KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,       KC_F,       KC_G,    KC_H,    KC_J,       KC_K,       KC_L,     KC_SCLN,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSFT,
-        KC_LCTL,                   MO(FUNCTN), MO(NUMBRS), KC_SPC,  KC_SPC,  MO(NUMBRS), MO(FUNCTN),                     KC_RALT
+   [0] = LAYOUT_2u(
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+        CTL_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        KC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+        KC_LCTL,                   KC_LALT, LT(1,KC_ENT),     LT(2,KC_SPC),     MO(3),                     KC_LGUI
     ),
-    [FUNCTN] = LAYOUT_1u(
-        KC_VOLU, KC_Q,    KC_PGUP, RGB_TOG,    BL_STEP,    KC_T,    KC_Y,    KC_U,       KC_UP,      RGB_MOD,  RGB_RMOD, KC_DEL,
-        KC_VOLD, KC_HOME, KC_PGDN, KC_D,       KC_U,       KC_MINS, KC_EQL,  KC_LEFT,    KC_DOWN,    KC_RIGHT, KC_QUOT,  RESET,
-        KC_LSFT, RGB_HUI, RGB_HUD, RGB_SAI,    RGB_SAD,    KC_LBRC, KC_RBRC, RGB_VAI,    RGB_VAD,    KC_DOT,   KC_BSLS,  KC_RSFT,
-        KC_LCTL,                   KC_TRNS,    KC_TRNS,    KC_SPC,  KC_SPC,  KC_TRNS,    KC_TRNS,                        KC_TRNS
+   [1] = LAYOUT_2u(
+        _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
+        _______, _______,C(KC_F4),A(KC_F4), _______, _______, KC_MINS, KC_EQL , KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV ,
+        _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
+        _______,                   _______,      _______,         _______,      _______,                   _______
     ),
-    [NUMBRS] = LAYOUT_1u(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,      KC_F5,      KC_F6,   KC_F7,   KC_F8,      KC_F9,      KC_F10,   KC_F11,   KC_F12,
-        KC_MUTE, KC_1,    KC_2,    KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,     KC_0,     KC_ENT,
-        KC_CAPS, KC_EXLM, KC_AT,   KC_HASH,    KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN,  KC_RPRN,  KC_RSFT,
-        KC_LCTL,                   KC_TRNS,    KC_TRNS,    KC_SPC,  KC_SPC,  KC_TRNS,    KC_TRNS,                        KC_TRNS
+   [2] = LAYOUT_2u(
+        _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP, KC_HOME,
+        _______, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_END ,
+        _______,                   _______,      _______,         _______,      _______,                   _______
     ),
+   [3] = LAYOUT_2u(
+        RESET  , RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, KC_F24 , _______,
+        _______, RGB_MOD, RGB_VAI, _______, _______, _______, _______, _______, _______, DS_SRED, DS_PRUN, KC_PGUP,
+        _______, RGB_RMOD,RGB_VAD, _______, _______, _______, _______, _______, _______, _______, DS_NXUN, KC_PGDN,
+        _______,                   _______,      _______,         _______,      _______,                   _______
+    )
 };
+
+bool spongebob_mode = false;
+bool spongebob_case = false;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (spongebob_mode) {
+    switch(keycode) {
+      case KC_A...KC_Z:
+        if (record->event.pressed) {
+          (spongebob_case ^= 1) == 0 ? tap_code16(S(keycode)) : tap_code(keycode);
+          return false; break;
+        }
+    }
+  }
+  switch(keycode) {
+    case KC_F23:
+      if (record->event.pressed) {
+        layer_leds ^= 1;
+        writePinLow(F5);
+        writePinLow(F6);
+        writePinLow(F7);
+      }
+      return false; break;
+    case KC_F24:
+      if (record->event.pressed) {
+        spongebob_mode ^= 1;
+        spongebob_mode ? writePinHigh(F5) : writePinLow(F5);
+      }
+      return false; break;
+  }
+  return true;
+}
