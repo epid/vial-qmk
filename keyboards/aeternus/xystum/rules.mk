f95e1e48eb5f05e883bf00f24f4e3acc39a5286b
Jared G <jared@crypto.cx>
2021-09-01 08:19:59 -0500

    Enabled encoder

--- keyboards/aeternus/xystum/rules.mk
+++ keyboards/aeternus/xystum/rules.mk
@@ -22,6 +22,7 @@ UNICODE_ENABLE = no        # Unicode
 BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no          # Audio output on port C6
 FAUXCLICKY_ENABLE = no     # Use buzzer to emulate clicky switches
+ENCODER_ENABLE = yes
 
 WS2812_DRIVER = bitbang
 # AUDIO_DRIVER = pwm_software
